8266be8b23adeb2fdd4cf5705c8048d2b812e97c
aluca94@gmail.com <LukaTizic>
2022-03-19 15:35:08 +0100

    JS fundamentals challenge #1

--- challenges.js
+++ challenges.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const weightMark = 78;
+const heightMark = 1.69;
+const weightJohn = 92;
+const heightJohn = 1.95;
+
+const markBMI = weightMark / heightMark ** 2;
+const johnBMI = weightJohn / heightJohn ** 2;
+const markBMIHigher = markBMI > johnBMI;
+console.log(markBMI, johnBMI, markBMIHigher);
+
+if (markBMI > johnBMI) {
+  console.log('Mark has higher BMI');
+} else {
+  console.log('John has higher BMI');
+}
