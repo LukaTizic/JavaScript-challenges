e92da8f432687247e793770726d77c6e1266a11f
aluca94@gmail.com <LukaTizic>
2022-03-19 22:19:49 +0100

    finished

--- challenge.js/challenge.4.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const bill = 430;
-const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
-console.log(
-  `This bill was ${bill}, tip was ${tip} and the total value is ${bill + tip}!`
-);
--- challenge4.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const bill = 275;
-const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
-console.log(
-  `The bill was ${bill}, the tip was ${tip}, and the total value is ${
-    bill + tip
-  }`
-);
